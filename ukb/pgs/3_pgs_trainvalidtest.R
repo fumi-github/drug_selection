b2b637c7838d9cab3705f7e83e0f076d19f9102a
fumi-github <fumihiko@takeuchi.name>
2025-09-27 18:04:40 +1000

    Refactor helper functions

--- ukb/pgs/3_pgs_trainvalidtest.R
+++ ukb/pgs/3_pgs_trainvalidtest.R
@@ -6,6 +6,9 @@ library(data.table)
 library(pROC)
 library(R2ROC)
 
+# HELPER FUNCTIONS
+# ---
+# A function to Winsorize a numeric vector, capping extreme values at the 1st and 99th percentiles.
 winsorize = function (x) {
   xmax = quantile(x, 0.99, na.rm = TRUE)
   xmin = quantile(x, 0.01, na.rm = TRUE)
@@ -13,6 +16,91 @@ winsorize = function (x) {
   x[!is.na(x) & x < xmin] = xmin
   return(x)
 }
+
+# A function to split data into training, validation, and test sets.
+splittrainvalidtest = function(n, ptest=0.2, stest=1, ptrain=0.6, strain=1) {
+  # Set seed for reproducible test set split.
+  set.seed(stest)
+  shuffled_indices = sample(n)
+  
+  # Assign indices to the test set.
+  test_indices = sort(head(shuffled_indices, round(n*ptest)))
+  trainvalid_indices = tail(shuffled_indices, n - round(n*ptest))
+  
+  # Set seed for reproducible train/validation split.
+  set.seed(strain)
+  trainvalid_indices = sample(trainvalid_indices)
+  
+  # Assign indices to the training and validation sets.
+  train_indices = sort(head(trainvalid_indices, round(n*ptrain)))
+  valid_indices = sort(tail(trainvalid_indices, length(trainvalid_indices) - round(n*ptrain)))
+  
+  return(list(train=train_indices, valid=valid_indices, test=test_indices))
+}
+
+# A function to train a two-step model: 1) a base model with covariates, 2) a penalized model on the residuals.
+train_two_step_model = function(outcome_variable, covariate_df, genotype_df, glmnets=0.04) {
+  # Step 1: Fit a binomial GLM with covariates to get the base model.
+  base_model = glm(outcome_variable ~ .,
+                   data=covariate_df,
+                   family=binomial(),
+                   na.action=na.exclude)
+  
+  # Extract residuals from the base model, handling NAs appropriately.
+  residuals_from_base_model = naresid(base_model$na.action, base_model$residual)
+
+  # The user has the option to winsorize residuals, currently commented out.
+  # residuals_from_base_model = winsorize(residuals_from_base_model)
+  
+  # Remove subjects with missing genotype data or residuals.
+  complete_cases_mask = (rowSums(is.na(genotype_df)) == 0 & !is.na(residuals_from_base_model))
+  table(complete_cases_mask)
+  
+  non_na_residuals = residuals_from_base_model[complete_cases_mask]
+  non_na_genotypes = genotype_df[complete_cases_mask, ]
+  
+  # Mean-center the genotype data.
+  for (i in 1:ncol(non_na_genotypes)) { 
+    non_na_genotypes[, i] = non_na_genotypes[, i] - mean(non_na_genotypes[, i])
+  }
+  
+  # Step 2: Fit a penalized (LASSO/elastic net) model using genotypes to predict the residuals.
+  pgs_model_fit = glmnet(
+    x = as.matrix(non_na_genotypes),
+    y = non_na_residuals)
+  
+  # Extract SNP coefficients for the specified lambda (penalty parameter).
+  snp_coefficients = as.numeric(coef(pgs_model_fit, s=glmnets))[-1]
+  
+  # Return coefficients from both steps.
+  return(list(base_model_coeffs=coef(base_model), pgs_coeffs=snp_coefficients))
+}
+
+# A function to cut a continuous variable into deciles.
+decilecut = function(score) {
+  # Calculate decile quantiles.
+  quantiles = quantile(score, seq(0, 1, 0.1), na.rm=TRUE)
+  
+  # If there are not enough unique quantile values (due to ties), add a small amount of noise to break ties.
+  if (length(unique(quantiles)) < 11) {
+    min_diff = min(diff(sort(unique(score)))) / 2
+    set.seed(1)
+    score_jittered = score + runif(length(score), max=min_diff)
+    quantiles = quantile(score_jittered, seq(0, 1, 0.1), na.rm=TRUE)
+  }
+  return(cut(score, quantiles, include.lowest=TRUE))
+}
+
+# A function to calculate the proportion of cases per decile of a score.
+decileproportion = function(score, outcome) {
+  proportions_by_decile = data.frame(decile = decilecut(score), trait = outcome) %>%
+    na.omit() %>%
+    group_by(decile) %>%
+    summarize(proportion = sum(trait=="1") / n(), .groups="drop")
+  return(proportions_by_decile$proportion)
+}
+
+
 
 data = read.table("ukb_imp_chr1_v3_qc.snps.raw", header=TRUE, sep="\t")
 # data = read.table("ukb_imp_chr1_v3_qc.withHF.snps.raw", header=TRUE, sep="\t")
@@ -45,86 +133,6 @@ data = data[, -c(1:2)]
 pheno = pheno[, -c(1:2)]
 covariates = covariates[, -c(1:2)]
 
-
-### split train test
-splittrainvalidtest = function(n, ptest=0.2, stest=1, ptrain=0.6, strain=1) {
-  set.seed(stest)
-  x = sample(n)
-  test = sort(head(x, round(n*ptest)))
-  trainvalid = tail(x, n - round(n*ptest))
-  set.seed(strain)
-  trainvalid = sample(trainvalid)
-  train = sort(head(trainvalid, round(n*ptrain)))
-  valid = sort(tail(trainvalid, length(trainvalid) - round(n*ptrain)))
-  return(list(train=train, valid=valid, test=test))
-}
-
-glm2steps = function(trait, covariates, data, glmnets=0.04) {
-  a0 = glm(trait ~ .,
-           data=covariates,
-           family=binomial(),
-           na.action=na.exclude)
-  y = naresid(a0$na.action, a0$residual)
-  # y = winsorize(y)
-  
-  keep = (rowSums(is.na(data)) == 0 & !is.na(y))
-  table(keep)
-  y2 = y[keep]
-  data2 = data[keep, ]
-  for (i in 1:ncol(data2)) { data2[, i] = data2[, i] - mean(data2[, i])}
-  
-  a2 = glmnet(
-    x = as.matrix(data2),
-    y = y2)
-  coefSNP = as.numeric(coef(a2, s=glmnets))[-1]
-  
-  # # relaxed LASSO
-  # a3 = lm(
-  #   y ~ x,
-  #   data=list(x = as.matrix(data2) %*% diag((coefSNP!=0)* 1),
-  #   y = y2))
-  # coefSNPb = coef(a3)[-1]
-  # coefSNPb[is.na(coefSNPb)] = 0
-  
-  # a2 = linearRidge(
-  #   y ~ .,
-  #   data = cbind(data.frame(y = y2), data2))
-  # coefSNP = coef(a2)[-1]
-  
-  # # 1 step; NG aborts
-  # x = cbind(data.frame(y = phenotrain$trait), datatrain)
-  # x = x[rowSums(is.na(x)) == 0, ]
-  # a2 = logisticRidge(
-  #   y ~ .,
-  #   data = x,
-  #   # scaling="corrForm")
-  #   scaling="none")
-  
-  
-  # # rescale
-  # a3 = lm(y ~ x, data=data.frame(y = y2, x = as.matrix(data2) %*% coefSNP))
-  # coefSNP = coefSNP * coef(a3)[2]
-  
-  return(list(coef1=coef(a0), coef2=coefSNP))
-}
-
-decilecut = function(x) {
-  q = quantile(x, seq(0, 1, 0.1), na.rm=TRUE)
-  if (length(unique(q)) < 11) {
-    delta = min(diff(sort(unique(x)))) / 2
-    set.seed(1)
-    x = x + runif(length(x), max=delta)
-    q = quantile(x, seq(0, 1, 0.1), na.rm=TRUE)
-  }
-  return(cut(x, q, include.lowest=TRUE))
-}
-decileproportion = function(x, trait) {
-  x = data.frame(decile = decilecut(x), trait = trait) %>%
-    na.omit() %>%
-    group_by(decile) %>%
-    summarize(proportion = sum(trait=="1") / n(), .groups="drop")
-  return(x$proportion)
-}
 
 assess2steps = function(trait, covariates, data, coef1, coef2) {
   combined = data.frame(
@@ -219,15 +227,15 @@ for (t in c("C10AA")) { # "C10AA", "C10AB", "C10AX09" "C02"
                           
                           # phenotrain$trait = phenotrain[, t]
                           
-                          x = glm2steps(phenotrain[, t], covariatestrain, datatrain, glmnets=glmnets)
+                          x = train_two_step_model(phenotrain[, t], covariatestrain, datatrain, glmnets=glmnets)
                           
                           
                           res = data.table::data.table(trait = t, glmnets=glmnets, stest = stest, strain = strain,
-                                                       coef1=list(x$coef1[1:4]), coef2=list(x$coef2))
+                                                       coef1=list(x$base_model_coeffs[1:4]), coef2=list(x$pgs_coeffs))
                           
-                          r = assess2steps(phenovalid[, t], covariatesvalid[, c(1:3)], datavalid, x$coef1[1:4], x$coef2)
+                          r = assess2steps(phenovalid[, t], covariatesvalid[, c(1:3)], datavalid, x$base_model_coeffs[1:4], x$pgs_coeffs)
                           res = cbind(res, r)
-                          # r = assess2steps(phenovalid[, t], covariatesvalid[, c(1:3)], datavalid, x$coef1[1:4], x$coef2b)
+                          # r = assess2steps(phenovalid[, t], covariatesvalid[, c(1:3)], datavalid, x$base_model_coeffs[1:4], x$pgs_coeffsb)
                           # res = cbind(res, r)
                           return(res)
                         })
