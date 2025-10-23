de3d2fb1e92b99a3cf5ee6e3af990bfb1c8a32cd
fumi-github <fumihiko@takeuchi.name>
2025-10-23 17:21:28 +1100

    Update input and output filenames

--- ukb/pgs/5_write_pgs_coeff.R
+++ ukb/pgs/5_write_pgs_coeff.R
@@ -23,18 +23,20 @@ rsallele = colnames(genotype_data)[-c(1:2)]
 # a = readRDS("aucR2ROC_lipidaemiadrug_exclsupplement/auc.C10AA.PC.wowinsorize.rds") %>% filter(glmnets==0.006)
 # a = readRDS("aucR2ROC_lipidaemiadrug_exclsupplement/auc.C10AB.PC.wowinsorize.rds") %>% filter(glmnets==0.08)
 # a = readRDS("aucR2ROC_lipidaemiadrug_exclsupplement/auc.C10AX09.PC.wowinsorize.rds") %>% filter(glmnets==0.03)
-a = readRDS("aucR2ROC_lipidaemiadrug_onlyaffectedexclsupplement.NGdecilecut/auc.C10AA.PC.wowinsorize.rds") %>% filter(glmnets==0.006)
-a = readRDS("aucR2ROC_lipidaemiadrug_onlyaffectedexclsupplement.NGdecilecut/auc.C10AB.PC.wowinsorize.rds") %>% filter(glmnets==0.09)
-a = readRDS("aucR2ROC_lipidaemiadrug_onlyaffectedexclsupplement.NGdecilecut/auc.C10AX09.PC.wowinsorize.rds") %>% filter(glmnets==0.05) 
+a = readRDS("aucR2ROC_lipidaemiadrug_onlyaffectedexclsupplement/auc.C10AA.PC.wowinsorize.rds") %>% filter(glmnets==0.006)
+a = readRDS("aucR2ROC_lipidaemiadrug_onlyaffectedexclsupplement/auc.C10AB.PC.wowinsorize.rds") %>% filter(glmnets==0.09)
+a = readRDS("aucR2ROC_lipidaemiadrug_onlyaffectedexclsupplement/auc.C10AX09.PC.wowinsorize.rds") %>% filter(glmnets==0.05) 
 
 # a = readRDS("aucR2ROC_hypertensiondrug_DBPge100SBPge160.withHF/auc.C03.PC.wowinsorize.rds") %>% filter(glmnets==0.01)
 # a = readRDS("aucR2ROC_hypertensiondrug_DBPge100SBPge160.withHF/auc.C07.PC.wowinsorize.rds") %>% filter(glmnets==0.005)
 # a = readRDS("aucR2ROC_hypertensiondrug_DBPge100SBPge160.withHF/auc.C08.PC.wowinsorize.rds") %>% filter(glmnets==0.004)
 # a = readRDS("aucR2ROC_hypertensiondrug_DBPge100SBPge160.withHF/auc.C09.PC.wowinsorize.rds") %>% filter(glmnets==0.002)
-a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160.NGdecilecut/auc.C03.PC.wowinsorize.rds") %>% filter(glmnets==0.01)
-a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160.NGdecilecut/auc.C07.PC.wowinsorize.rds") %>% filter(glmnets==0.005)
-a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160.NGdecilecut/auc.C08.PC.wowinsorize.rds") %>% filter(glmnets==0.004)
-a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160.NGdecilecut/auc.C09.PC.wowinsorize.rds") %>% filter(glmnets==0.002)
+a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160/auc.C03.PC.wowinsorize.rds") %>% filter(glmnets==0.01)
+a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160/auc.C07.PC.wowinsorize.rds") %>% filter(glmnets==0.002)
+a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160/auc.C08.PC.wowinsorize.rds") %>% filter(glmnets==0.004)
+a = readRDS("aucR2ROC_hypertensiondrug_onlyaffectedDBPge100SBPge160/auc.C09.PC.wowinsorize.rds") %>% filter(glmnets==0.003)
+
+i="C09"
 
 x = a %>% select(stest, coef2, pgscoeff)
 x = x %>% group_by(stest) %>%
@@ -45,13 +47,13 @@ x = colMeans(x)
 res = as.data.frame(x)
 res$rs = sub("_.*", "", rsallele)
 res$allele = sub(".*_", "", rsallele)
-write.table(res[, c(2:3, 1)], file="coeff.C10AX09.txt",
+write.table(res[, c(2:3, 1)], file=paste0("coeff.", i, ".txt"),
             row.names=F, col.names=F, sep=" ", quote=FALSE)
 
 x = a %>% select(coef1)
 x = colMeans(do.call(rbind, x[[1]]))
 res = as.data.frame(x)
-write.table(res, file="coeff_basal.C10AX09.txt",
+write.table(res, file=paste0("coeff_basal.", i, ".txt"),
             row.names=T, col.names=F, sep=" ", quote=FALSE)
 
 ### aou
@@ -59,7 +61,7 @@ write.table(res, file="coeff_basal.C10AX09.txt",
 snpsukbaou = read.table("snps_ukb_aou.txt", sep="\t", header=TRUE)
 # snpsukbaou = read.table("snps_ukb_aou.withHF.txt", sep="\t", header=TRUE)
 
-res = read.table("coeff.C10AX09.txt", sep=" ")
+res = read.table(paste0("coeff.", i, ".txt"), sep=" ")
 
 identical(sort(snpsukbaou$ID), sort(res$V1))
 res$IDaou = snpsukbaou$IDaou[match(res$V1, snpsukbaou$ID)]
@@ -68,5 +70,5 @@ res$IDaou = snpsukbaou$IDaou[match(res$V1, snpsukbaou$ID)]
 x = (res$V1 %in% c("rs7948851"))
 res$V3[x] = - res$V3[x] 
 
-write.table(res[, c(4, 2:3)], file="coeff.C10AX09.hg38.txt",
+write.table(res[, c(4, 2:3)], file=paste0("coeff.", i, ".hg38.txt"),
             row.names=F, col.names=F, quote=F)
